43fccb27c33a32be010af4ec787071de5000b9d8
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-09-02 18:26:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 // write your code below!
 
-function happyHolidays()
-{
-  return "Happy holidays!"
+function happyHolidays() {
+  return "Happy holidays!";
 }
 
-function happyHolidaysTo(name)
-{
-  return "Happy Holidays, " + name
+function happyHolidaysTo(name) {
+  return "Happy holidays, " + name + "!";
 }
 
-function happyHolidaysTo(holiday,name)
-{
-  return "Happy " + holiday + ", " + name
+function happyHolidayTo(holiday, name) {
+  return "Happy " + holiday + ", " + name + "!";
 }
 
-function holidayCountdown(days,holiday)
-{
-  return "It's " + days + " until " + holiday + "!"
+function holidayCountdown(holiday, days) {
+  return "It's " + days + " days until " + holiday + "!";
 }
